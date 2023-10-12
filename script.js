a2dcd173264bc6f06d75acc71d5f78b352bfe9e2
PMorgane <passarellimorgane43@gmail.com>
2023-10-12 19:01:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggle_img(id) {
+    var img = document.getElementById(id);
+    if(img.style.display == "none") {
+      img.style.display = "inline";
+    } else {
+      img.style.display = "none";
+    }
+  }
+/*
+  function toggle_text(id) {
+    var span = document.getElementById(id);
+    if(span.style.display == "none") {
+      span.style.display = "inline";
+    } else {
+      span.style.display = "none";
+    }
+  }*/
